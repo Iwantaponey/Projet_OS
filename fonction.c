750cc0fa9f6c70c2cfc1ab4ce1097c565923ffdf
Juju <juju0397@msn.com>
2017-04-06 13:40:14 +0200

    fonction init_mot et init_traitement

--- fonction.c
+++ fonction.c
@@ -14,9 +14,24 @@
 
 mot init_mot(pthread_t tid, int num, int nb_char)
 {
-	mot m = 
+	mot w = 
 	{tid, num, nb_char, malloc(nb_char*sizeof(char))};
+	return w;
+}
+
+message init_mess(pid_t pid, int num, int nb_mots)
+{
+	message m=
+	{pid, num, nb_mots, malloc(nb_mots*sizeof(mot))};
 	return m;
+}
+
+traitement init_traitement(char chiffrement, int cle, int nb_messages, char ** chemin)
+{
+	
+	traitement t=
+	{chiffrement, cle, nb_messages, chemin };
+	return t;
 }
 
 traitement extraire (traitement t, char ** parametres)
