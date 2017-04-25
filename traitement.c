f54eda0522f563ec12ccc2ac38a5b6cce70ba5ad
Juju <juju0397@msn.com>
2017-04-26 00:00:30 +0200

    cryptage fait

--- traitement.c
+++ traitement.c
@@ -2,20 +2,30 @@
 
 char cryptage(char c, int cle)
 {
-	if((c < 65) || ((c > 90) && (c < 97)) || (c > 122)) return c; // Test si on est bien dans les lettres minuscules ou majuscules uniquement
-	c = c + cle; // Modification du caractère
-	printf ("%c\n", c);
-	if((c > 90) || (c > 122))  c = c - 26; // Ligne qui fait de la merde quand avec un p en entrée mais pas avec un z par exemple
-	printf ("%c\n", c);
+	if (c < 65) return c; // avant A
+	if (c > 122) return c; // après z
+	if (c > 90 && c < 97) return c; // entre Z et a
+	if (c < 91) // si je suis une majuscule comprise entre 65 et 90
+	{
+		printf("majuscule \n");
+		c = c + cle; // Modification du caractère
+		if (c > 9) c=c-26;
+	}
+	else if (c > 96) // si je suis une minuscule comprise entre 97 et 122 
+	{
+		printf("minuscule \n");
+		c = c + cle; // Modification du caractère
+		if (c > 122) c=c-26;
+	}
 	return c;
 }
 
-int main()
+int main(int argc, char ** argv)
 {
 	int cle = 3; // Test avec une clé de cryptage à 3
-	char c = 'p'; // Caractère à modifier
+	char c = argv[1][0]; // Caractère à modifier
 	char d = cryptage(c, cle);
-	printf ("%c\n", d);
+	printf ("%c \n", d);
 	return 0;
 }
 
